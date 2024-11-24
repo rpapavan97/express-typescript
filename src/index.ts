d9e488f767d918f4c61ce896e3f9af54acc7439a
Pavan Kumar <77329032+rpapavan97@users.noreply.github.com>
2024-11-24 16:07:40 +0530

    feat: setup Express.js TypeScript boilerplate

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { name, version } from "../package.json";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.json({ app_name: name, version });
+});
+
+app.get('/health', (_req: Request, res: Response) => {
+  res.json({ isHealthy: true });
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
